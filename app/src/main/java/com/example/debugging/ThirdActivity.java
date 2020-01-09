65051366a69aeca30b61ca8bfe9d069949582dc7
Nagy Norbert <nagynorbi97@gmail.com>
2020-01-09 19:07:58 +0100

    Feladat elkészítése

---
+++ app/src/main/java/com/example/debugging/ThirdActivity.java
@@ -0,0 +1,33 @@
+package com.example.debugging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    private Button buttonBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        init();
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent next = new Intent(ThirdActivity.this,Menu.class);
+                startActivity(next);
+                finish();
+            }
+        });
+    }
+
+    public void init()
+    {
+        buttonBack = findViewById(R.id.buttonBack);
+    }
+}
